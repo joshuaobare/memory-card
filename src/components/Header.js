32aa3b4eee54e4b2e99cfa9d6d63e75403d15450
joshuaobare <jobare.obare@gmail.com>
2022-09-14 12:05:35 +0300

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <h1 className="title">Memory Card</h1>
+                <div className="scores">
+                    <span>Scores: 0</span>
+                    <span>Best Score:</span>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
