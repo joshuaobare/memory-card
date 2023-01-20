26ea2b2f8fd849f45fd6984878363b8093f45dce
joshuaobare <jobare.obare@gmail.com>
2023-01-20 19:41:43 +0300

    moved characters to a separate file and added gameEnd state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
-import React , {useState } from 'react';
+import React , {useEffect, useState } from 'react';
 import Header from "./components/Header"
 import Card from "./components/Card"
-import Aang from "./images/Aang.jpg"
-import Appa from "./images/Appa.jpg"
-import Azula from "./images/Azula.jpg"
-import Cabbage from "./images/Cabbagemerchant.jpg"
-import Iroh from "./images/Iroh.jpg"
-import Katara from "./images/Katara.jpg"
-import Momo from "./images/Momo.jpg"
-import Sokka from "./images/Sokka.jpg"
-import Toph from "./images/Toph.jpg"
-import Zuko from "./images/Zuko.jpg"
-import uniqid from "uniqid"
+import { characters } from './characters';
+
 
 function App() {
    
-  const characters = [{
-      name:"Aang", image:Aang, id:uniqid()
-  },{ name:"Appa", image:Appa, id:uniqid()
-  },{ name:"Azula", image:Azula,id:uniqid()
-  },{ name:"Cabbage Merchant", image:Cabbage, id:uniqid()
-  },{ name:"Iroh",image:Iroh, id:uniqid()
-  },{ name:"Katara",image:Katara, id:uniqid()
-  },{ name:"Momo",image:Momo, id:uniqid()
-  },{ name:"Sokka",image:Sokka, id:uniqid()
-  },{ name:"Toph",image:Toph, id:uniqid()
-  },{ name:"Zuko",image:Zuko, id:uniqid()
-  }]
- 
-  const [click , setClick] = useState([])
+  const [click , setClick] = useState([]) 
+  const [chars , setChars] = useState(characters)
+  const [score , setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(score)
+  const [gameEnd , setGameEnd] = useState(false) 
+  
+  useEffect(() => {
+    if(score === 10){
+      setGameEnd(true)
+    }
+  },[score])
+
+  useEffect(() => {
+    if(gameEnd){
+      
+    }
+  },[gameEnd])
 
   const shuffleArray = array => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -40,13 +34,7 @@ function App() {
 
     return array
   }
-
-  const [chars , setChars] = useState(characters)
-  const [score , setScore] = useState(0)
-  const [bestScore, setBestScore] = useState(score)
-
-  //useEffect(() => {},[click])
-
+  
   function handleClick(id) {
     setChars(prevState => {
       return [...shuffleArray(prevState)]
---
+++ src/characters.js
@@ -0,0 +1,26 @@
+import Aang from "./images/Aang.jpg"
+import Appa from "./images/Appa.jpg"
+import Azula from "./images/Azula.jpg"
+import Cabbage from "./images/Cabbagemerchant.jpg"
+import Iroh from "./images/Iroh.jpg"
+import Katara from "./images/Katara.jpg"
+import Momo from "./images/Momo.jpg"
+import Sokka from "./images/Sokka.jpg"
+import Toph from "./images/Toph.jpg"
+import Zuko from "./images/Zuko.jpg"
+import uniqid from "uniqid"
+
+const characters = [{
+    name:"Aang", image:Aang, id:uniqid()
+},{ name:"Appa", image:Appa, id:uniqid()
+},{ name:"Azula", image:Azula,id:uniqid()
+},{ name:"Cabbage Merchant", image:Cabbage, id:uniqid()
+},{ name:"Iroh",image:Iroh, id:uniqid()
+},{ name:"Katara",image:Katara, id:uniqid()
+},{ name:"Momo",image:Momo, id:uniqid()
+},{ name:"Sokka",image:Sokka, id:uniqid()
+},{ name:"Toph",image:Toph, id:uniqid()
+},{ name:"Zuko",image:Zuko, id:uniqid()
+}]
+
+export {characters}
