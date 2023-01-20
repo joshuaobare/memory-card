8e6d169b2a6177ee4f86ac4793e8a7c93328b023
joshuaobare <jobare.obare@gmail.com>
2023-01-20 21:29:47 +0300

    added coloring and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# memory-card
+# Memory Card
+
+A fun and simple game to test your memory.
+Don't click the same character twice!
+
+[Live Demo](https://joshuaobare.github.io/memory-card/)
+
+## Built with
+- React JS
+- Material UI
+- React Confetti
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
                 <h1 className="title">Memory Card</h1>
                 <div className="scores">
                     <div>Score: {props.score}</div>
-                    <div>Best Score: {props.bestScore}</div>
+                    <div>High Score: {props.bestScore}</div>
                 </div>
             </header>
         </div>
